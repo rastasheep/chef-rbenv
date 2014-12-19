3a47baaac58bc144b39892aaaa9b5ff8f9fbea4f
Aleksandar Diklic <rastasheep3@gmail.com>
2014-12-19 15:53:32 +0100

    Update system_install.rb

--- recipes/system_install.rb
+++ recipes/system_install.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-include_recipe 'rbenv'
+include_recipe 'rbenv-deprecated'
 
 upgrade_strategy  = build_upgrade_strategy(node['rbenv']['upgrade'])
 git_url           = node['rbenv']['git_url']
